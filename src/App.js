b32086cce64b7e4637b0a22a05904f31ad56cd6f
LriKrj <lauri.karjalainen3@hotmail.com>
2022-04-05 15:54:29 +0300

    tehtävä tehty

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
-import React, { useState }  from 'react'
-import { AgGridReact } from 'ag-grid-react';
-
-import './App.css';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import React, { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "./App.css";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import DateFnsUtils from "@date-io/date-fns";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 function App() {
-  const [todo, setTodo] = useState({desc: '', date: '', priority: ''});
+  const [todo, setTodo] = useState({ desc: "", date: "", priority: "" });
   const [todos, setTodos] = useState([]);
 
   const addTodo = (event) => {
     setTodos([...todos, todo]);
-    setTodo({desc: '', date: '', priority: ''});
-  }
+    setTodo({ desc: "", date: "", priority: "" });
+  };
 
   const inputChanged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  } 
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
 
   const columns = [
-    { field: 'desc', sortable: true, filter: true },
-    { field: 'date', sortable: true, filter: true },
-    { field: 'priority', sortable: true, filter: true },
-  ]
+    { field: "desc", sortable: true, filter: true },
+    { field: "date", sortable: true, filter: true },
+    { field: "priority", sortable: true, filter: true },
+  ];
 
   return (
     <div className="App">
-      <input name="desc" value={todo.desc} onChange={inputChanged}/>
-      <input name="date" value={todo.date} onChange={inputChanged}/>
-      <input name="priority" value={todo.priority} onChange={inputChanged}/>
-      <button onClick={addTodo}>Add</button>
-      <div className="ag-theme-material" style={{height: 400, width: 600, margin: 'auto'}}>
-        <AgGridReact
-          rowData={todos}
-          columnDefs={columns}>
-        </AgGridReact>
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DatePicker
+            label="Date"
+            name="date"
+            value={todo.date}
+            onChange={(time) => setTodo({ ...todo, date : time })}
+          />
+        </MuiPickersUtilsProvider>
+        <TextField
+          label="Description"
+          variant="standard"
+          name="description"
+          value={todo.desc}
+          onChange={inputChanged}
+        />
+        <TextField
+          label="Priority"
+          variant="standard"
+          name="prio"
+          value={todo.priority}
+          onChange={inputChanged}
+        />
+        <Button onClick={addTodo} variant="contained">
+          Add event
+        </Button>
+      </Stack>
+      <div
+        className="ag-theme-material"
+        style={{ height: 400, width: 600, margin: "auto" }}
+      >
+        <AgGridReact rowData={todos} columnDefs={columns}></AgGridReact>
       </div>
     </div>
   );
